9b74c39f7396e30df1243775a6ce565dbc6842f9
rahul <rhvarrier@gmail.com>
2021-11-11 20:40:28 +0100

    Adding persist-scratch, mutiple-cursors; fix error in package init

--- custom-functions.el
+++ custom-functions.el
@@ -3,3 +3,13 @@
   (setq custom-file "~/.emacs.d/custom.el")
   (load-file custom-file)
   )
+
+(defun delete-current-line ()
+  "Delete (not kill) the current line."
+  (interactive)
+  (save-excursion
+    (delete-region
+     (progn (forward-visible-line 0) (point))
+     (progn (forward-visible-line 1) (point)))))
+
+(global-set-key (kbd "<C-S-backspace>") 'delete-current-line)
